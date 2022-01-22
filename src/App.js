4c45600b252e92d36f7b79e94e294210056aa58e
dudkin-v <dudkin31v@gmail.com>
2022-01-22 19:48:28 +0200

    added show more button, fetching data from API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { useState, useEffect } from 'react';
+
 import './App.styles.css';
 
 function App() {
+const [limit, setLimit] = useState(10);
+const [images, setImages] = useState([]);
+
+useEffect(async () => {
+    const response = await fetch(`https://picsum.photos/v2/list?limit=${limit}`);
+    const data = await response.json();
+    setImages(data);
+}, [limit]);
+
+const showMore = () => setLimit(limit + 10);
+
   return (
     <div className="app">
       <h1 className='app-heading'>Images gallery</h1>
-    
+      <button className='showMore-btn' onClick={showMore}>Show more...</button>
     </div>
   );
 }
