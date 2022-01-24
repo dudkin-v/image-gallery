0bedcdc589620f80f8cfa10844ccdc65d86b61c6
dudkin-v <dudkin31v@gmail.com>
2022-01-24 18:01:25 +0200

    added close button for popup

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -4,12 +4,16 @@ import './Popup.styles.css';
 
 const Popup = ({ image, popupVisible, onClose }) => {
     return (
-        <div className={popupVisible ? 'popup-wrapper-active' : ''} onClick={onClose}>
-           <div className={popupVisible ? 'popup-active' : 'popup-hiden'}>
-               <h2>{`Author: ${image.author}`}</h2>
-               <h2>{`Width: ${image.width}`}</h2>
-               <h2>{`Height: ${image.height}`}</h2>
-           </div>
+        <div className='popup'>
+           <div className={popupVisible ? 'popup-wrapper-active' : ''} onClick={onClose}></div>
+              <div className={popupVisible ? 'popup-active' : 'popup-hiden'}>
+                  <div className='popup-description'>
+                     <h2>{`Author: ${image.author}`}</h2>
+                     <h2>{`Width: ${image.width}`}</h2>
+                     <h2>{`Height: ${image.height}`}</h2>
+                  </div>
+                  <button className='popup-close-btn' onClick={onClose}>X</button>
+              </div>
         </div>
     )
 }
